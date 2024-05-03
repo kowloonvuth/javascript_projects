38e277431737bd9da9fcd96670c937ea2fca415e
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-05-03 18:07:53 +0700

    twelve project

---
+++ countdown page/script.js
@@ -0,0 +1,91 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+
+// 11 is hour, 30 is minutes, and 0 is second
+const futureDate = new Date(tempYear, tempMonth, tempDay + 5, 11, 30, 0)
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+
+// Easy to access
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+// A bit hard to access
+
+let month = futureDate.getMonth();
+month = months[month];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+function getRemaindingTime() {
+    const today = new Date().getTime();
+    // t represent the millisecond number
+    const t = futureTime - today;
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    // calculate all value
+
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    // set values of array
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+        return (item < 10) ? `0${item}` : item;
+    }
+
+    items.forEach(function(item, index) {
+        item.innerHTML = format(values[index]);
+    });
+    
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+}
+
+
+//countdown
+
+let countdown = setInterval(getRemaindingTime, 1000);
+// set initial value
+getRemaindingTime();
